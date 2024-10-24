8f13bf51effd593f172bcca434b78e49a05c4a0a
Ernesto224 <ev402648@gmail.com>
2024-10-24 13:36:47 -0600

    Modificaciones de interfaz completas

--- Frontend_SOLITEL/src/app/shared/services/sub-modalidad.service.ts
+++ Frontend_SOLITEL/src/app/shared/services/sub-modalidad.service.ts
@@ -7,27 +7,24 @@ import { Observable } from 'rxjs';
 })
 export class SubModalidadService {
 
-  private urlServices: string = "https://localhost:7211/api/SubModalidad/";
-  private urlObtener: string = "obtenerSubModalidad";
-  private urlinsertar: string = "insertarSubModalidad";
-  private urleliminar: string = "eliminarSubModalidad";
+  private urlServices: string = "https://localhost:7211/api/SubModalidad";
 
   constructor(private http: HttpClient) { }
 
   public obtener = (): Observable<any[]> => {
-    return this.http.get<any[]>(`${this.urlServices}${this.urlObtener}`);
+    return this.http.get<any[]>(`${this.urlServices}`);
   }
 
-  public insertar = (objeto: any): Observable<any> => {
+  public insertar = (objeto: object): Observable<any> => {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'accept': 'text/plain'
     });
-    return this.http.post(`${this.urlServices}${this.urlinsertar}`, objeto, { headers });
+    return this.http.post(`${this.urlServices}`, objeto, { headers });
   }
 
   public eliminar = (id: number): Observable<any> => {
-    return this.http.delete(`${this.urlServices}${this.urleliminar}/${id}`, {
+    return this.http.delete(`${this.urlServices}/${id}`, {
       headers: { 'accept': 'text/plain' }
     });
   }
